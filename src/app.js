12dfbe6dbbe9b390c758ef706dd2726c029f6da9
Shin H <nobunegi13@gmail.com>
2021-05-28 21:16:10 +0900

    [change]Vueの記載方法をVue3に統一した。
    VueのバージョンをVue3に定めたためコンポーネントの記法をVue3の記法に変更した。

--- src/app.js
+++ src/app.js
@@ -1,17 +1,23 @@
+const { createApp } = Vue;
+
 const StartPage = {
+  // 最終的にページ全体を1コンポーネントにする。そのコンポーネント内にパーツ化したコンポーネントを読み込む
   template: `
   <div class="text_area w-auto mx-4 mx-md-5 text-white">
       <p class="p-3">vimコマンド練習アプリ</p>
+      <!-- 難易度選択部分は別コンポーネントに切り出す予定 -->
       <p class="px-3">難易度選択</p>
       <div class="m-3">
-        <label class="difficulty_choose_label mb-4">
-          <input type="radio" value="易しい" />
-          <span class="text-white difficulty_level">易しい</span>
-        </label>
-        <label class="difficulty_choose_label">
-          <input type="radio" value="難しい" />
-          <span class="text-white difficulty_level">難しい</span>
-        </label>
+        <form name="difficult">
+          <label class="difficulty_choose_label mb-4">
+            <input type="radio" value="易しい" />
+            <span class="text-white difficulty_level">易しい</span>
+          </label>
+          <label class="difficulty_choose_label">
+            <input type="radio" value="難しい" />
+            <span class="text-white difficulty_level">難しい</span>
+          </label>
+        </form>
       </div>
     </div>
   `,
@@ -24,10 +30,11 @@ const PrimaryButton = {
   </div>`,
 };
 
-new Vue({
-  el: "#main",
+const app = createApp({
   components: {
     "main-contents": StartPage,
     "primary-button": PrimaryButton,
   },
 });
+// app.use(router);
+window.vm = app.mount("#app");
--- src/header.js
+++ src/header.js
@@ -1,3 +1,5 @@
+
+// ヘッダーコンポーネントはapp.jsに移動予定
 const headerTemplate = `
   <div class="p-3 text-center">
     <slot name="header"></slot>
