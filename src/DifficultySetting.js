61d45cdbb80b0393cb6a8048d8d8060b653a9884
morimori-coder <67218438+morimori-coder@users.noreply.github.com>
2021-06-06 15:39:01 +0900

    問題の正誤判定ができるようになった

--- src/DifficultySetting.js
+++ src/DifficultySetting.js
@@ -1,32 +1,33 @@
 /////////////////////  難易度関連(start)  //////////////\
 // 初期画面で選択状態のラジオボタンを取得する
-// 今回はラジオボタンから難易度が取れればそれで十分
 
 // 難易度を入れておく変数(ラジオボタンのラベル)
 let difficultyValue;
 
 // 出題する問題と解答を入れる変数
-let questions;
+let questionArray;
 
 // 「問題スタート」ボタンをクリックしたときに呼び出される関数
 function clickedStartProblems() {
     difficultyCheck();
 
-
     // 難易度に合わせて問題を格納
+    // allProbemはgetJson.jsのグローバル変数
     // allProbemの中身を見るとわかるが、0番目がeasy、1番目がdifficultとなっている
     switch (difficultyValue) {
         case '易しい':
-            questions = allProblem[0].questions;
+            questionArray = allProblem[0].questions;
             break;
 
         case '難しい':
-            questions = allProblem[1].questions;
+            questionArray = allProblem[1].questions;
             break;
 
         default:
         // どちらでもない場合にする処理を入れる(今のところ処理内容は未定)
     }
+
+    arrayShuffle(questionArray);
 
     // problemChange();
 }
@@ -45,7 +46,8 @@ function difficultyCheck(){
             difficultyValue = document.difficult[i].value;
 
             // このalertはリリース時には削除する予定
-            alert(difficultyValue + "が選択されました。");
+            // リリース前だけどうっとしいのでコメントアウト
+            // alert(difficultyValue + "が選択されました。");
 
             // flagがtrueになった時点でfor文を抜ける
             break;
--- src/Question.js
+++ src/Question.js
@@ -1,15 +1,62 @@
-
-////////////// Json //////////////
-// 初期画面が表示された瞬間にすべての難易度のJsonファイルを取得する
-// 問題スタート」ボタンが押下された瞬間に初期画面のラジオボタンを判定する
-// ラジオボタンに応じたJsonファイルの中身をQuestions配列に持たせる
-// このタイミングでQuestions配列の順番をランダムに入れ替える
-
-
-function someFunc (){
-     let textLabel = document.getElementsByClassName('button-primary p-2 text-white');
+function someFunc() {
+    let textLabel = document.getElementsByClassName('button-primary p-2 text-white');
 
     //  textLabel.item = 'hoge';
+}
+
+let currentQuestion;
+let currentAnswer;
+
+// 配列をランダムに並び変える
+const arrayShuffle = (array) => {
+    for (var i = (array.length - 1); 0 < i; i--) {
+
+        // 0〜(i+1)の範囲で値を取得
+        var r = Math.floor(Math.random() * (i + 1));
+
+        // 要素の並び替えを実行
+        var tmp = array[i];
+        array[i] = array[r];
+        array[r] = tmp;
+
+    }
+    currentQuestion = array[0].question;
+    currentAnswer = array[0].answer;
+}
+
+// 解答を判定
+const judgeAnswer = () => {
+    // 入力された解答を取得
+    let anserArea = document.getElementById("answerArea")
+    let inputedAnswer = anserArea.value;
+    let extraKey = "";
+
+    // 選択されている特殊キーを取得
+    for (var i = 0; i < document.extra_keys.length; i++) {
+        // i番目のラジオボタンがチェックされているかを判定
+        if (document.extra_keys[i].checked) {
+            extraKey = document.extra_keys[i].value;
+            // ラジオボタンのため、選択された値が取れた時点でfor文を抜ける
+            break;
+        }
+    }
+
+    // 答え合わせの後に表示する解答
+    let displayAnswer = currentAnswer;
+
+    // 特殊キーが存在する場合は、displayAnserの表示を「Ctrl + z」みたいにしたいので成形している
+    let anserExtraKey = currentAnswer.match(/Ctrl|Shift|Esc/g);
+    if (anserExtraKey != null) {
+        displayAnswer = currentAnswer.replace(anserExtraKey[0], anserExtraKey[0] + " + ");
+    }
+
+    // \nは改行コード
+    if (currentAnswer === (extraKey + inputedAnswer)) {
+        anserArea.value = "正解！" + "\n" + displayAnswer + " : " + currentQuestion;
+    }
+    else {
+        anserArea.value = "不正解！" + "\n" + displayAnswer + " : " + currentQuestion;
+    }
 }
 
 // javascriptの変数って任意のタイミングで解放できるのか調べても良いかも
