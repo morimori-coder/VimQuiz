1b99da442cddb8e4efc8aa5935dc79b3f2fa2d79
Shin H <nobunegi13@gmail.com>
2021-06-18 09:11:42 +0900

    [reafafctor]ヘッダーをコンポーネントとしてapp,jsに移設した。

--- src/app.js
+++ src/app.js
@@ -1,3 +1,13 @@
+const VimHeader = {
+  template:`
+    <div class="p-3 text-center">
+      <div class="header_tittle my-3 py-3 mx-auto text-white mw-100px">
+        <h1 class="display-3">Vim道</h1>
+      </div>
+    </div>
+  `
+}
+
 const app = new Vue({
   el: "#app",
   data: {
@@ -21,4 +31,7 @@ const app = new Vue({
       this.answer_check = true;
     },
   },
+  components: {
+    'vim-header': VimHeader
+  }
 });
